fdbdb73b49afc9d71f7c48a568d315dad9599af2
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-30 15:27:51 -0400

    Update style.css

--- src/modules/showscorelist.js
+++ src/modules/showscorelist.js
@@ -8,12 +8,16 @@ const showscorelist = (gameScore) => {
     // Create a line div to put inside name and score
     const line = document.createElement('div');
     line.setAttribute('class', 'linelist');
-    // Create p element to inject name and score
-    const namescore = document.createElement('p');
-    namescore.setAttribute('class', 'name');
-    namescore.innerHTML = `${gameScore[i].user} : ${gameScore[i].score}`;
-    // Append namescore to line and line to list
-    line.appendChild(namescore);
+    // Create p elements to inject name and score
+    const name = document.createElement('p');
+    name.setAttribute('class', 'name');
+    const score = document.createElement('p');
+    score.setAttribute('class', 'score');
+    score.innerHTML = `${gameScore[i].score}`;
+    name.innerHTML = `${gameScore[i].user} : `;
+    // Append name and score to line and line to list
+    line.appendChild(name);
+    line.appendChild(score);
     list.appendChild(line);
   }
 };
